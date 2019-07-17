457c3fe44358b5e5af606af22e17dfef645f20b4
Rehman Ali <rehmanali001@gmail.com>
2019-07-17 20:23:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,10 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
-  
+def countdown_with_sleep(number)
+    number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
 end
 end
